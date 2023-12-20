aba6539ea339f8c2ab4772e97cc257e5b16565cf
Karan samrat <karansamrat05039@gmail.com>
2023-12-20 10:31:51 +0530

    formating of file is done

--- src/App.js
+++ src/App.js
@@ -1,80 +1,50 @@
 import React, { useState } from "react";
 import "./App.css";
 import AddTodo from "./AddTodo";
-import TodoItems from "./ItemContaineer";
-
-
+import TodoItems from "./TodoItems";
 
 function App() {
-  let time  = new Date();
-
-
+  let time = new Date();
 
   const intialTodoItem = [];
 
+  const [todoItems, setTodoItems] = useState(intialTodoItem);
 
-
-
-
-  const [todoItems , setTodoItems] = useState(intialTodoItem)
-
-  const handelNewItem = (itemName , itemDate ) =>{
+  const handelNewItem = (itemName, itemDate) => {
     console.log(`new itemm added: ${itemName} DATE: ${itemDate} `);
 
-    const newTodoItems=[...todoItems ,{id:Math.random(),
-    name: itemName,
-    dueDate: itemDate,
-  }]
+    const newTodoItems = [
+      ...todoItems,
+      { id: Math.random(), name: itemName, dueDate: itemDate },
+    ];
 
-  setTodoItems(newTodoItems);
-
+    setTodoItems(newTodoItems);
   };
 
-  const handelDeleteItem = (todoItemName) =>{
-
-
-    const newTodoItemDelete = todoItems.filter( item => item.name !== todoItemName)
-     setTodoItems(newTodoItemDelete)
+  const handelDeleteItem = (todoItemName) => {
+    const newTodoItemDelete = todoItems.filter(
+      (item) => item.name !== todoItemName
+    );
+    setTodoItems(newTodoItemDelete);
 
     console.log(`to do item deleted ${todoItemName}`);
-
-  }
-
-
-
-
+  };
 
   return (
     <div className="App">
+      <center className="todo-container">
+        <h1>Todo App</h1>
+        <div className="container text-center mt-5">
+          <AddTodo onNewItem={handelNewItem} />
 
-    <center className="todo-container" >
+          {todoItems.length === 0 && "sorry no content"}
 
-      <h1>Todo App</h1>
-      <div className="container text-center mt-5">
-      
+          <TodoItems addItem={todoItems} onDeleteClick={handelDeleteItem} />
 
-        <AddTodo onNewItem ={handelNewItem}  />
-
-        {todoItems.length===0 && "sorry no content"}
-      
-    
-  
-    <TodoItems addItem={todoItems} onDeleteClick = {handelDeleteItem} />
- 
-
-
-
-
-
-{/* -------------------------------------------- */}
-   <h2 className="mt-5">this is the {time.toLocaleTimeString()}</h2>
-    
-      </div>
-
-
-    </center>
-
-
+          {/* -------------------------------------------- */}
+          <h2 className="mt-5">this is the {time.toLocaleTimeString()}</h2>
+        </div>
+      </center>
     </div>
   );
 }
